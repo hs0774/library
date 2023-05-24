63e0f682e9cb0ea78b17bf9ef1ea2f25cff58624
Hardeep Singh <hs0774@gmail.com>
2023-05-24 16:09:40 -0400

    Project is complete

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ const title = document.getElementById('title');
 const author = document.getElementById('author');
 const pages = document.getElementById('Pages');
 const read = document.getElementById('Read');
+const mainContent = document.querySelector('.mainContent');
+const form=document.querySelector('.form');
 
 
 const clear = function(){
@@ -25,19 +27,15 @@ btn2.addEventListener('click', function() {
     clear();
 });
 
-btn3.addEventListener('click', function(event) {
+form.addEventListener('submit', function(event) {
     event.preventDefault();
+
     const titleSaved = title.value;
-    console.log(titleSaved);
     const authorSaved = author.value;
-    console.log(authorSaved);
     const pagesSaved = pages.value;
-    console.log(pagesSaved);
     const readSaved = read.checked;
-    console.log(readSaved);
     
     addBookToLibrary(titleSaved,authorSaved,pagesSaved,readSaved);
-    
     modalContainer.classList.toggle('hidden');
     clear();
     
@@ -53,5 +51,80 @@ function Book(book,author,page,read) {
 }
 
 function addBookToLibrary(titleSaved,authorSaved,pagesSaved,readSaved) {
-  myLibrary.push(new Book(titleSaved,authorSaved,pagesSaved,readSaved));
+
+  const book= new Book(titleSaved,authorSaved,pagesSaved,readSaved);
+  myLibrary.push(book);
+
+  const div = document.createElement('div');
+  div.className='dummybox';
+  mainContent.appendChild(div);
+
+  const bookId = 'book-' + Date.now();
+  div.setAttribute('data-id', bookId);
+
+  const p1 = document.createElement('p');
+  const p2 = document.createElement('p');
+  const p3 = document.createElement('p');
+  const label = document.createElement('label');
+  const input = document.createElement('input');
+  const remove = document.createElement('div');
+  const removebutton = document.createElement('button');
+
+  remove.className='remove';
+  removebutton.className='removee';
+  remove.appendChild(removebutton);
+
+  removebutton.textContent='Remove';
+  p1.textContent = `Title: ${titleSaved}`;
+  p2.textContent =`Author: ${authorSaved}`;
+  p3.textContent =`# of pages: ${pagesSaved}`;
+
+  if(readSaved){
+     input.checked=true;
+  }
+
+  label.textContent = 'Read:';
+  label.htmlFor = `CustomRead-${bookId}`;
+  input.id = `CustomRead-${bookId}`;
+  input.type = 'checkbox'; 
+
+  input.addEventListener('change', function(event) {
+    const checkbox = event.target;
+    const boxId = checkbox.parentElement.parentElement.getAttribute('data-id');
+    
+    
+    const bookIndex = myLibrary.findIndex(function(book) {
+      return 'book-' + book.id === boxId;
+     });
+    
+
+     if (bookIndex !== -1) {
+      myLibrary[bookIndex].read = checkbox.checked;
+     }
+     });
+
+  removebutton.addEventListener('click', function(event) {
+    const button = event.target;
+    const boxId = button.parentElement.parentElement.getAttribute('data-id');
+    
+
+    const bookIndex = myLibrary.findIndex(function(book) {
+      return 'book-' + book.id === boxId;
+    });
+    
+
+     if (bookIndex !== -1) {
+      myLibrary.splice(bookIndex, 1);
+    }
+    
+
+     div.remove();
+     });
+
+  label.appendChild(input);
+  div.appendChild(p1);
+  div.appendChild(p2);
+  div.appendChild(p3);
+  div.appendChild(label);
+  div.appendChild(remove);
 }
